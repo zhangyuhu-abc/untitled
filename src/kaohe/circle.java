650ff43f72415299bbdacbf1322379e68f42648b
zhangyuhu-abc <212115646@qq.com>
2026-03-09 20:07:52 +0800

    Update circle.java

--- src/kaohe/circle.java
+++ src/kaohe/circle.java
@@ -3,6 +3,10 @@ package kaohe;
 public class circle extends shape{
     private double area;
 
+     public void setArea(double area) {
+        this.area = area;
+    }
+
     public circle() {
     }
 
